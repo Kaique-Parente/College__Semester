0cfdc47ace6b8f2428598acb563d4b01a15a9b82
Kaique Parente <kaique.parente.game@outlook.com>
2024-04-04 13:39:05 -0300

    Itens Pedidos

---
+++ Banco de Dados/AulaSQL 03-04/itenspedidos.sql
@@ -0,0 +1,10 @@
+USE loja;
+
+INSERT INTO itenspedidos(codigoProduto, codigoPedidos, quantidade) VALUES
+(01, 01, 100),
+(02, 02, 15),
+(03, 03, 50),
+(04, 04, 10),
+(05, 05, 30);
+
+SELECT * FROM itenspedidos;
