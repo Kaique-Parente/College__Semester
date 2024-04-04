2c7f3fe3578981f6274a3eeea2c32d3bdbf80a7c
Kaique Parente <kaique.parente.game@outlook.com>
2024-04-04 13:41:23 -0300

    Vendedores

---
+++ Banco de Dados/AulaSQL 03-04/vendedores.sql
@@ -0,0 +1,13 @@
+use loja;
+
+insert into vendedores(codigoVendedor, nome, salarioFixo) values 
+(01, 'João da Silva', 2500.99),
+(02, 'Maria da Silva', 5000),
+(03, 'Pedro Joaquin', 4500),
+(04, 'Marcio Bolivia', 3500),
+(05, 'Joana Silva', 7600);
+
+SELECT * FROM vendedores;
+
+
+
